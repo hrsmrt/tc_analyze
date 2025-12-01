e31e7770b3f19ff484421895adcb7d35cb5ec8b8
Hiroshi Murata <31mh2019@gmail.com>
2025-12-01 13:20:42 +0900

    Fix ss_wind10m_max_plot.py: Add missing data_type='fig' parameter
    問題: 画像が保存されない
    原因: get_domain_path()にdata_type='fig'が指定されていなかった
    修正: data_type='fig'を追加して、正しい図の保存先を指定

--- analysis/whole_domain/2d/plot/ss_wind10m_max_plot.py
+++ analysis/whole_domain/2d/plot/ss_wind10m_max_plot.py
@@ -14,18 +14,24 @@ mpl_style_sheet = parse_style_argument()
 
 # 設定の初期化
 config = AnalysisConfig()
+output_folder = config.get_domain_path("whole_domain", "2d/ss_wind10m_max", data_type="fig")
+os.makedirs(output_folder, exist_ok=True)
 
-folder = config.get_domain_path("whole_domain", "2d/ss_wind10m_max", data_type="fig")
-os.makedirs(folder, exist_ok=True)
+ss_u10m = np.fromfile(f"{config.input_folder}ss_u10m.grd", dtype=">f4").reshape(
+    config.nt, config.ny, config.nx
+)
+ss_v10m = np.fromfile(f"{config.input_folder}ss_v10m.grd", dtype=">f4").reshape(
+    config.nt, config.ny, config.nx
+)
 
-# データの読み込み
-data = np.load(os.path.join(config.get_domain_path("whole_domain", "2d/ss_wind10m_max"), "ss_wind10m_max.npy"))
+data_abs = np.sqrt(ss_v10m**2 + ss_u10m**2)
+data_abs_max = data_abs.max(axis=(1, 2))
 
 # プロット
 plt.style.use(mpl_style_sheet)
 fig, ax = plt.subplots(figsize=(4, 3))
-ax.plot(config.time_list[1:], data[1:])
+ax.plot(config.time_list[1:], data_abs_max[1:])
 ax.set_xlabel("時間 [hour]")
 ax.set_ylabel("最大10m風速 [m/s]")
-fig.savefig(os.path.join(folder, "ss_wind10m_max.png"))
+fig.savefig(os.path.join(output_folder, "ss_wind10m_max.png"))
 plt.close()
